7e0cafc1cb1da4110f107ddcfce7b050a23cb414
jessicaa-li <55930140+jessicaa-li@users.noreply.github.com>
2020-11-07 16:34:41 -0500

    pushing diary entry

--- src/components/DiaryEntry.jsx
+++ src/components/DiaryEntry.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import {convertToColour} from '../ColourConversion';
 import './DiaryEntry.css';
 
 function DiaryEntry(props){
+    let entrySyle = {
+        backgroundColor: convertToColour (props.mood)
+    }
     return (
-    )
+        <div className="DiaryEntry" style = {entrySyle}> 
+            <h2 className="date">{props.date}</h2>
+            <h3 className="entry">Your Diary Entry: </h3>
+            <p className="entryText">{props.entry}</p>
+            <h3 className="score">Today's Mood: </h3>
+            <p className="scoreText">{props.mood}</p>
+            <h3 className="magnitude">Today's Magnitude:</h3>
+            <p className="magnitudeText">{props.magnitude}</p>
+        </div>
+    );
 }
 
 export default DiaryEntry;
