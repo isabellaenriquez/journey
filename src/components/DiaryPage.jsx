042b940c9720fcb8e29fbcfc42df49011cecc4e3
Truman <56100185+trumanbe01@users.noreply.github.com>
2020-11-07 15:43:07 -0500

    Added firebase interactions

--- src/components/DiaryPage.jsx
+++ src/components/DiaryPage.jsx
@@ -1,18 +1,30 @@
 import React, { useState, useRef } from 'react';
 import './DiaryPage.css';
+import firebase from './Firebase.js';
 
 function DiaryPage(props){
 
+    const date = "November 7th";
     const entry = useRef("");
 
     function addEntry(event) {
         console.log(entry.current.value);
-    }
+        const db = firebase.firestore();
+        db.settings({
+            timestampInSnapshots: true
+        });
+        const userRef = db.collection("users").doc(date).set({
+            date: "November 7th",
+            entry: entry.current.value,
+            magnitude: 1,
+            mood: 1
+        });
+    };
 
     return (
         <div className="DiaryPage">
             <header className="title">How you feeling?</header>
-            <p className="date">November 7th, 2020</p>
+            <p className="date">{date}</p>
             <textarea className="entry" ref={entry} placeholder="Placeholder" />
             <button className="addButton" onClick={addEntry}>Add Entry</button>
         </div>
