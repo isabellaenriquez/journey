09c6cb7e7da437d905b8ed6caeda42e2d2ed6894
jessicaa-li <55930140+jessicaa-li@users.noreply.github.com>
2020-11-07 18:33:07 -0500

    new colours!

--- src/ColourConversion.js
+++ src/ColourConversion.js
@@ -1,9 +1,9 @@
 exports.convertToColour = function (score) {
-    if (score >= 3) return "#66FF00";
-    else if (score >= 2) return "#99FF00";
-    else if (score >= 1) return "#CCFF00";
-    else if (score >= 0) return "#FFFF00";
-    else if (score >= -1) return "#FFCC00";
-    else if (score >= -2) return "#FF9900";
-    else if (score >= -3) return "#FF6600";
+    if (score >= 3) return "#43f751";
+    else if (score >= 2) return "#99ff72";
+    else if (score >= 1) return "#ccff72";
+    else if (score >= 0) return "#f5f578";
+    else if (score >= -1) return "#ffcc72";
+    else if (score >= -2) return "#ffad72";
+    else if (score >= -3) return "#e87272";
 }
