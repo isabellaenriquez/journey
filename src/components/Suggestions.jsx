5f42ef919ad98e3e497396b6a7d5aa3aae54416e
Isabella Enriquez <45607721+riquezen@users.noreply.github.com>
2020-11-07 13:44:46 -0500

    Added tips

--- src/components/Suggestions.jsx
+++ src/components/Suggestions.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import './Suggestions.css';
+import { tips } from './tips.json';
 
-function Suggestions(props){
+function Suggestions(props) {
+
+
+
+    function pickTip() {
+        const tipCount = tips.length;
+        let randomTip = Math.floor(Math.random() * tipCount);
+        return tips[randomTip];
+    }
+
     return (
-    )
+        <div className="Suggestions">
+            <div className="Resources">
+                <h1>Resources</h1>
+                <p></p>
+            </div>
+            <div className="Tip">
+                <h1>Tip!</h1>
+                <p>{pickTip()}</p>
+            </div>
+        </div>
+    );
 }
+
+
 
 export default Suggestions;
