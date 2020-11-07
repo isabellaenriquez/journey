9a2742627640b7ee9c306fee9c5c7155bf5f928d
Isabella Enriquez <45607721+riquezen@users.noreply.github.com>
2020-11-07 16:40:13 -0500

    Added resources

--- src/components/Suggestions.jsx
+++ src/components/Suggestions.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Suggestions.css';
 import { tips } from './tips.json';
+import { resources, crisis } from './resources.json';
 
 function Suggestions(props) {
 
@@ -12,15 +13,77 @@ function Suggestions(props) {
         return tips[randomTip];
     }
 
+    function tailorResources(score) {
+        // make sure to get the user's data
+        let list = "";
+        let i;
+        if (score < 0) {
+            // add crisis resources 
+            for (i = 0; i < crisis.length; i++) {
+                list += '<div className="resource">';
+                
+                if (crisis[i].web){
+                    list+= '<a href="' + crisis[i].web + '">'
+                }
+    
+                list += '<h4>' + crisis[i].name + '</h4>';
+    
+                if (crisis[i].web){
+                    list+='</a>';
+                }
+
+                list += '<p>' + crisis[i].description + '</p>';
+                if (crisis[i].phone) {
+                    list += '<p>Call ' + crisis[i].phone + '</p>';
+                }
+                if (crisis[i].text) {
+                    list += '<p>Text ' + crisis[i].text + '</p>';
+                }
+                list += '</div>'
+            }
+        }
+        for (i = 0; i < resources.length; i++) {
+            list += '<div className="resource">';
+            
+            if (resources[i].web){
+                list+= '<a href="' + resources[i].web + '">'
+            }
+
+            list += '<h4>' + resources[i].name + '</h4>';
+
+            if (resources[i].web){
+                list+='</a>';
+            }
+            list += '<p>' + resources[i].description + '</p>';
+            if (resources[i].phone) {
+                list += '<p>Call ' + resources[i].phone + '</p>';
+            }
+            if (resources[i].text) {
+                list += '<p>Text ' + resources[i].text + '</p>';
+            }
+            list += '</div>'
+        }
+        return list;
+    }
+
+    const resourceList = tailorResources(-0.1); // tailor resources depending on average mood
+
     return (
         <div className="Suggestions">
+            <div className="Tip">
+                <table>
+                    <tr>
+                        <td>
+                <h1>Tip</h1>
+                            <p>{pickTip()}</p></td>
+                        <td><img src="https://cdn1.iconfinder.com/data/icons/light-bulb-emoticons/512/Happy-Emoji-Emotion-Face-Expression-Feeling_1-512.png"></img>
+                        </td>
+                    </tr>
+                </table>
+            </div>
             <div className="Resources">
                 <h1>Resources</h1>
-                <p></p>
-            </div>
-            <div className="Tip">
-                <h1>Tip!</h1>
-                <p>{pickTip()}</p>
+                <div className="content" dangerouslySetInnerHTML={{ __html: resourceList }}></div>
             </div>
         </div>
     );
