679ee7f7b9bd829630c2628fc0a7d16859eb6839
Marc Kevin Quijalvo <33074023+mkevinq@users.noreply.github.com>
2020-11-07 18:18:20 -0500

    first version of final app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import firebase, { getAll } from './components/Firebase.js';
+import Graph from './components/Graph';
+import DiaryPage from './components/DiaryPage';
+import DiaryEntry from './components/DiaryEntry';
+import Suggestions from './components/Suggestions';
 
 function App() {
+  const [entries, setEntries] = useState([]);
+
+  function addEntry(entry) {
+    setEntries(oldEntries => [entry, ...oldEntries]);
+    console.log(entries);
+  }
+
+  const db = firebase.firestore();
+
+  db.settings({
+      timestampInSnapshots: true
+  });
+
+  useEffect(() => {
+    getAll(db)
+    .then((response) => {
+      console.log(response);
+      setEntries(response);
+    })
+  }, [db])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DiaryPage onNewEntry={addEntry}></DiaryPage>
+      <Graph entries={entries}></Graph>
+      { (entries.length > 0) &&
+        <>
+          <div>
+            {
+              entries.map((entry) => (<DiaryEntry key={entry.day + "/" + (entry.month+1) + "/" + entry.year} day={entry.day} month={entry.month} year={entry.year} entry={entry.entry} mood={entry.mood} magnitude={entry.magnitude}></DiaryEntry>))
+            }
+          </div>
+          <Suggestions score={entries[0].mood}></Suggestions>
+        </>
+      }
     </div>
   );
 }
