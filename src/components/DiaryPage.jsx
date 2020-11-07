fdd9e263a6c215bb4780e664845a0d68c4fdd275
Truman <56100185+trumanbe01@users.noreply.github.com>
2020-11-07 13:17:37 -0500

    Added the code for the diary page

--- src/components/DiaryPage.jsx
+++ src/components/DiaryPage.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './DiaryPage.css';
 
 function DiaryPage(props){
+
+    const entry = useRef("");
+
+    function addEntry(event) {
+        console.log(entry.current.value);
+    }
+
     return (
-    )
+        <div className="DiaryPage">
+            <header className="title">How you feeling?</header>
+            <p className="date">November 7th, 2020</p>
+            <textarea className="entry" ref={entry} placeholder="Placeholder" />
+            <button className="addButton" onClick={addEntry}>Add Entry</button>
+        </div>
+    );
 }
 
 export default DiaryPage;
