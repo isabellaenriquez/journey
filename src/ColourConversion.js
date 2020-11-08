50abc799634397bc7c2759770c2724d56157ad84
jessicaa-li <55930140+jessicaa-li@users.noreply.github.com>
2020-11-07 20:05:08 -0500

    changing colour specifications

--- src/ColourConversion.js
+++ src/ColourConversion.js
@@ -1,9 +1,9 @@
 exports.convertToColour = function (score) {
-    if (score >= 3) return "#43f751";
-    else if (score >= 2) return "#99ff72";
-    else if (score >= 1) return "#ccff72";
-    else if (score >= 0) return "#f5f578";
-    else if (score >= -1) return "#ffcc72";
-    else if (score >= -2) return "#ffad72";
-    else if (score >= -3) return "#e87272";
+    if (score >= 0.8) return "#43f751";
+    else if (score >= 0.5) return "#99ff72";
+    else if (score >= 0.3) return "#ccff72";
+    else if (score >= 0.1) return "#f5f578";
+    else if (score >= -0.1) return "#ffcc72";
+    else if (score >= -0.4) return "#ffad72";
+    else if (score <= -0.6) return "#e87272";
 }
